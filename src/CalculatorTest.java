aa9ad3b02d1f38d60554c085a45968cb66ca41c4
kinleycummins2 <kc0928941@otc.edu>
2025-01-23 12:15:20 -0600

    Completed, all tests passed - Changed tests to be just "void", forgot that an access modifier is not needed with JUnit5

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -7,7 +7,7 @@ class CalculatorTest
 
     //Test for adding positive numbers
     @Test
-    public void testAddPositives()
+    void testAddPositives()
     {
         assertEquals(5, calc.add(3, 2), "3 + 2 should be 5");
     }
@@ -21,80 +21,80 @@ class CalculatorTest
 
     //Test for adding two negative numbers
     @Test
-    public void testAddNegatives()
+    void testAddNegatives()
     {
         assertEquals(-15, calc.add(-10, -5), "-10 + -5 should be -15");
     }
 
     //Test for subtracting two positives
     @Test
-    public void testSubtractPositives()
+    void testSubtractPositives()
     {
         assertEquals(3, calc.subtract(5, 2), "5 - 2 should be 3");
     }
 
     //Test for subtracting a positive and a negative number
     @Test
-    public void testSubtractPositiveAndNegative()
+    void testSubtractPositiveAndNegative()
     {
         assertEquals(-12, calc.subtract(-10, 2), "-10 - 2 should be -12");
     }
     //Test for subtracting two negatives
     @Test
-    public void testSubtractNegatives()
+    void testSubtractNegatives()
     {
         assertEquals(0, calc.subtract(-4, -4), "-4 - -4 should be 0");
     }
 
     //Test for multiplying positives
     @Test
-    public void testMultiplyPositives()
+    void testMultiplyPositives()
     {
         assertEquals(12, calc.multiply(3, 4), "3 * 4 should be 12");
     }
 
     //Test for adding a positive and a negative number
     @Test
-    public void testMultiplyPositiveAndNegative()
+    void testMultiplyPositiveAndNegative()
     {
         assertEquals(-10, calc.multiply(5, -2), "5 * -2 should be -10");
     }
 
     //Test for multiplying negatives
     @Test
-    public void testMultiplyNegatives()
+    void testMultiplyNegatives()
     {
         assertEquals(12, calc.multiply(-6, -2), "-6 * -2 should be 12");
     }
 
     //Test for multiplying by 0
     @Test
-    public void testMultiplyByZero()
+    void testMultiplyByZero()
     {
         assertEquals(0, calc.multiply(5, 0), "5 * 0 should be 0");
     }
 
     //Test for dividing positives
     @Test
-    public void testDividingPositives()
+    void testDividingPositives()
     {
         assertEquals(2, calc.divide(4, 2), "4 / 2 should be 2");
     }
     //Test for dividing positive and negative numbers
     @Test
-    public void testDividingPositiveAndNegative()
+    void testDividingPositiveAndNegative()
     {
         assertEquals(-2, calc.divide(10, -5), "10 / -5 should be -2");
     }
     //Test for dividing negatives
     @Test
-    public void testDividingNegatives()
+    void testDividingNegatives()
     {
         assertEquals(6, calc.divide(-12, -2), "-12 / -2 should be 6");
     }
     //Test for dividing by zero - expecting exception
     @Test
-    public void testDividingByZeroThrowsException()
+    void testDividingByZeroThrowsException()
     {
         assertThrows(ArithmeticException.class, () -> calc.divide(10, 0), "10 / 0 should throw an ArithmeticException");
     }
